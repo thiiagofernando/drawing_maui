18056a5f0cb18d50a347caf08b0a2961f5588fcd
Thiago Fernando <thiago.fernando@msn.com>
2024-02-06 14:00:28 -0300

    initial

---
+++ AppDrawing/AppDrawing/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppDrawing
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppDrawing/AppDrawing/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppDrawing
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
